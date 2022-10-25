06035eb45b93cb84ed013d234c3e335b05742eac
Md Masud Rana <mdmasude7@gmail.com>
2022-10-25 22:21:20 +0600

    auth context

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,7 +1,11 @@
 import React from "react";
+import { useContext } from "react";
 import { Link } from "react-router-dom";
+import { AuthContext } from "../../Context/UserContex";
 
 const Login = () => {
+  const userInfo = useContext(AuthContext);
+  // console.log(userInfo);
   return (
     <div>
       <div className="hero min-h-screen bg-base-200">
--- src/Context/UserContex.jsx
+++ src/Context/UserContex.jsx
@@ -1,17 +1,36 @@
 import React from "react";
+import { useState } from "react";
 import { createContext } from "react";
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import app from "../Firebase/Firebase.init";
 
 //
 //
 //
 //
-const AuthContext = createContext();
+export const AuthContext = createContext();
 
-const UserContex = () => {
+const auth = getAuth(app);
+
+const UserContex = ({ children }) => {
+  const [user, setUSer] = useState([]);
+
+  //
+  //
+  const LogIn = (email, password) => {
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+  //
+  //
+  //
+
+  const userInfo = "hello world";
   return (
     <div>
-      {/*  */}
-      {/*  */}
+      <AuthContext.Provider value={userInfo}>
+        {/*  */}
+        {children}
+      </AuthContext.Provider>
     </div>
   );
 };
